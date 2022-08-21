3f5fe8d3d1933664e1d8f6ca79feabda9f214f3b
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2022-08-21 19:12:04 -0400

    Update useQueryPosts.js

--- src/hooks/useQueryPosts.js
+++ src/hooks/useQueryPosts.js
@@ -13,15 +13,15 @@ export const useQueryPosts = () => {
         status,
     } = useInfiniteQuery(
         ['queryPosts'],
-        ({ pageParam = 9 }) => {
+        ({ pageParam = 0 }) => {
             return fetchPosts({start: pageParam, limit: maxPost})
         },
         {
             getNextPageParam : (lastPage, pages) => {
-                const page = (pages.length + 1) * maxPost - 1;                
-
-                if (pages[pages?.length - 1] ?.data?.result?.length !== 0) {
-
+            const page = pages[pages?.length - 1]?.data?.result[pages[pages?.length - 1]?.data?.result.length - 1]?. entry_id - 1;
+               
+            if (pages[pages?.length - 1]?.data?.result?.length !== 0 && page > 1) {
+                    console.log(page);
                     return page;
                 }
 

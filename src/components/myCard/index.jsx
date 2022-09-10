3a77a657cec5bf5678900fbadb9415ab807425f5
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2022-09-09 21:14:20 -0400

    feat: lazy load on myCard

--- src/components/myCard/index.jsx
+++ src/components/myCard/index.jsx
@@ -1,12 +1,13 @@
-import moment from 'moment';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
+import moment from 'moment'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
 import CardActionArea from '@mui/material/CardActionArea';
-import CardContent from '@mui/material/CardContent';
+import CardContent from '@mui/material/CardContent'
 
-import { Markdown } from '../markdown';
-import { PostStatics } from '../postStatics';
+import { useNearScreen } from '../../hooks/useNearScreen'
+import { Markdown } from '../markdown'
+import { PostStatics } from '../postStatics'
 import { MyLink } from '../mylink'
 import { MyAvatar } from '../avatar'
 
@@ -17,35 +18,38 @@ export const MyCard = (props) => {
         return JSON.parse(image)['thumbnails'] ? JSON.parse(image)['thumbnails'][0] : '';
     }
 
+    const { show, element } = useNearScreen();
+
     return (
         <Grid item="item" xs={12} md={12}>
             <MyAvatar type="small" />
             <MyLink to={'/' + permlink}>
-                <CardActionArea>
+                <CardActionArea ref={element}>
                     <Card sx={{ display: 'flex' }}>
-                        <CardContent sx={{ flex: 1 }}>
-                            <Typography component="h2" variant="h5">
-                                {title}
-                            </Typography>
-                            <Typography variant="subtitle1" color="text.secondary">
-                                {moment(date, "YYYYMMDD").locale('es').fromNow()}
-                            </Typography>
-                            <Typography variant="subtitle1" paragraph="paragraph">
-                                <Markdown
-                                    className="markdown"
-                                >
-                                    {description.substring(0, 500)}
-                                </Markdown>
-                            </Typography>
-                            <PostStatics
-                                votes={votes}
-                                comments={comments}
-                                amount={amount}
-                            />
-                        </CardContent>
+                        {
+                            show && <CardContent sx={{ flex: 1 }}>
+                                <Typography component="h2" variant="h5">
+                                    {title}
+                                </Typography>
+                                <Typography variant="subtitle1" color="text.secondary">
+                                    {moment(date, "YYYYMMDD").locale('es').fromNow()}
+                                </Typography>
+                                <Typography variant="subtitle1" paragraph="paragraph">
+                                    <Markdown className="markdown">
+                                        {description.substring(0, 500)}
+                                    </Markdown>
+                                </Typography>
+                                <PostStatics
+                                    votes={votes}
+                                    comments={comments}
+                                    amount={amount}
+                                />
+                            </CardContent>
+                        }
+
                     </Card>
                 </CardActionArea>
             </MyLink>
-        </Grid>
+        </Grid >
     );
 }

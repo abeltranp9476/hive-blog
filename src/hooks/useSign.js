173aa5b8e7abbf9dc23623440c34b9035d96f693
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2022-09-15 16:22:19 -0400

    feat: create useSign custom hook

---
+++ src/hooks/useSign.js
@@ -0,0 +1,18 @@
+import { tokenUtils } from '../utils/authentication'
+
+export const useSign = () => {
+    const search = window.location.search
+    const params = new URLSearchParams(search)
+    const expiresIn = params.get('expires_in')
+    let userName = params.get('username')
+    let token = params.get('access_token')
+
+    if (token && userName) {
+        tokenUtils.setAuthenticationToken(userName + ':' + token)
+    } else {
+        userName = tokenUtils.getUserId()
+        token = tokenUtils.getAuthenticationToken()
+    }
+
+    return { userName, token, expiresIn }
+}

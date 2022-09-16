1b06917653cbca04e3d215a6670d469c0040051b
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2022-09-16 16:42:04 -0400

    feat: add logout function to useSign custom hook

--- src/hooks/useSign.js
+++ src/hooks/useSign.js
@@ -1,4 +1,5 @@
 import { tokenUtils } from '../utils/authentication'
+import { useNavigate } from 'react-router-dom';
 
 export const useSign = () => {
     const search = window.location.search
@@ -6,6 +7,13 @@ export const useSign = () => {
     const expiresIn = params.get('expires_in')
     let userName = params.get('username')
     let token = params.get('access_token')
+
+    const navigate = useNavigate();
+
+    const logout = () => {
+        tokenUtils.clearAuthenticationToken()
+        navigate('/logout')
+    }
 
     if (token && userName) {
         tokenUtils.setAuthenticationToken(userName + ':' + token)
@@ -16,5 +24,5 @@ export const useSign = () => {
             : ''
     }
 
-    return { userName, token, expiresIn }
+    return { userName, token, expiresIn, logout }
 }

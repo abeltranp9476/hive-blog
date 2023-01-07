bb16187db4c7512455be7eaa6034be853868d7e2
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2023-01-07 10:27:53 -0500

    Update useSign.js

--- src/hooks/useSign.js
+++ src/hooks/useSign.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 
 import { tokenUtils } from '../utils/authentication'
 
